7db1c65d0fe1650493c150286c4f953469979db8
macrowil <macrow.co@gmail.com>
2024-02-12 18:19:00 -0700

    added search by category functionality, still bugged on first page load though

--- client/src/pages/views/ShopEZH.jsx
+++ client/src/pages/views/ShopEZH.jsx
@@ -1,11 +1,9 @@
-import React from 'react';
-import ConsumerNavBar from '../../components/navbar/ConsumerNavBar';
-import DropDownButtons from '../../components/shop/DropDownButtons';
-import ShopByCategory from '../../components/shop/ShopByCategory';
-
+import React from "react";
+import ConsumerNavBar from "../../components/navbar/ConsumerNavBar";
+import DropDownButtons from "../../components/shop/DropDownButtons";
+import ShopByCategory from "../../components/shop/ShopByCategory";
 
 export default function ShopEZH() {
-
   return (
     <>
       <ConsumerNavBar />
